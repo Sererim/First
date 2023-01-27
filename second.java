72438ffb8f05b7d65bcd38fad9bbae4a01e8d17f
Sererim <danielleymanw@gmail.com>
2023-01-27 17:43:17 +0300

    Initial commit of second and third exercies.

---
+++ second.java
@@ -0,0 +1,7 @@
+/*
+ * Check if entered year is a leap year.
+ */
+
+public class second {
+    
+}
---
+++ third.java
@@ -0,0 +1,11 @@
+/*
+ * Input:
+ * val = 3
+ * nums = [3, 2, 2, 3]
+ * Output:
+ * nums = [2, 2, 3, 3]
+ */
+
+public class third {
+    
+}
